d7b9be80f05c32c985f553f5637638de5c3833b9
RRBPerformance <romain.romeyerbouchard@gmail.com>
2026-09-08 18:32:12 +0200

    Change title from 'Académie Tennis' to 'RAPH'

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if 'db_soir' not in st.session_state:
     st.session_state['db_soir'] = pd.DataFrame(columns=['Date', 'Etat_Jour', 'Zones_Douleur_Soir', 'Type_Douleur'])
 
 # Titre principal
-st.markdown("<h1 style='text-align: center; color: #1E3A8A;'>🎾 Académie Tennis : Suivi de la Performance</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #1E3A8A;'>🎾 RAPH : Suivi de la Performance</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: #6B7280;'>Monitoring quotidien - Optimisation et Prévention</p>", unsafe_allow_html=True)
 st.divider()
 
